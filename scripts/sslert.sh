6829afb559dfcf67de9e9afc865d04e75a41f7fb
247662 Gieczyski Mateusz <mateusz.gielczynski@outlook.com>
2024-10-22 01:35:51 +0200

    add sslcert script

---
+++ scripts/sslert.sh
@@ -0,0 +1,3 @@
+asadmin set configs.config.server-config.http-service.http-listener.http-listener-2.ssl.cert-nickname=payara-ssl-cert
+asadmin set configs.config.server-config.http-service.http-listener.http-listener-2.ssl.key-store=/opt/payara/keystore.jks
+asadmin set configs.config.server-config.http-service.http-listener.http-listener-2.ssl.trust-store=/opt/payara/truststore.jks
