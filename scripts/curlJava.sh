4a5d4cdd31bc6a552f903a04592b0ad5d082483d
247662 Gieczyski Mateusz <mateusz.gielczynski@outlook.com>
2024-10-21 18:14:31 +0200

    add curls

---
+++ scripts/curlJava.sh
@@ -0,0 +1,11 @@
+mkdir /java
+cd /java
+
+curl -o jdk.tar.gz https://download.java.net/java/GA/jdk21.0.1/415e3f918a1f4062a0074a2794853d0d/12/GPL/openjdk-21.0.1_linux-x64_bin.tar.gz
+
+tar -xvzf jdk.tar.gz
+
+rm jdk.tar.gz
+
+echo "export JAVA_HOME=/java/jdk-21.0.1" >> /etc/profile
+echo "PATH=\$PATH:\$PAYARA_HOME/bin" >> /etc/profile
---
+++ scripts/curlMaven.sh
@@ -0,0 +1,11 @@
+mkdir /java
+cd /java
+
+curl -o maven.tar.gz https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+
+tar -xvzf maven.tar.gz
+
+rm maven.tar.gz
+
+echo "export MAVEN_HOME=/java/apache-maven-3.9.9" >> /etc/profile
+echo "PATH=\$PATH:\$MAVEN_HOME\bin" >> /etc/profile
